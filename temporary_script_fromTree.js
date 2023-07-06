03a7377dae060437a88b34562f885f65f3d4a7c0
Theresa Pauly <105754937+TreedServices@users.noreply.github.com>
2023-07-05 19:29:51 -0500

    Create temporary_script_fromTree.js
    to allow get and post to sql database on Azure (just playing around with the idea of php and odbc because it can be set within the js file)

---
+++ temporary_script_fromTree.js
@@ -0,0 +1,8 @@
+<?php
+    $user = "ArpaCloudInterns"; 
+    $password = "Inter2023";
+    $ODBCConnection = odbc_connect("Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlforwebsite.database.windows.net,1433;Database=SQL_website;Uid=CloudSA3320c6f6;Pwd={Inter2023};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+                                   
+
+  $SQLQuery = "SELECT * FROM autotest.dept";
+    $RecordSet = odbc_exec($ODBCConnection, $SQLQuery);
