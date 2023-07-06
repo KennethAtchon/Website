bedd2034ee3c3108bf7058938a0fed350a70a072
Finnar123 <kenneth.atchon@hotmail.com>
2023-07-06 13:28:12 -0500

    test

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
-// install jdbc package
+// install odbc package
+const odbc = require('odbc');
+// config odbc and add connection string to azure
+const connectionString = `Driver={ODBC Driver 18 for SQL Server};Server=tcp:sqlforwebsite.database.windows.net,1433;Database=SQL_website;Uid=admin1@internsarpacloud.onmicrosoft.com;Pwd=recipeforcooK123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;Authentication=ActiveDirectoryPassword`;
 
-// config jdbc and add connection string to azure
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 // ... Your server code ...
@@ -16,17 +20,33 @@ app.get('/', (req, res) => {
 
 // post statement will add info from website to db
 
-// console.log(req.body)
-
-  /*
+  
 app.post('/', async (req,res) => {
     
+  word = req.body.word
+
+  try {
+  const connection = await odbc.connect(connectionString);
+
+    // Execute the SQL query to insert data into the database
+    const query = `INSERT INTO DATA (WORD) VALUES (?)`;
+    await connection.query(query, [word ]);
+
+    console.log('Data inserted successfully');
+
+  //Close the database connection
+     connection.close();
+
+    
+  } catch (err) {
+    console.error('Error executing SQL query:', err);
+
+    
+  }
+
 
   })
-  */
-
-
-
+  
 
 
 
